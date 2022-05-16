3f5e312a75997131d8648fad4c34864d9938318a
Samir Atef <https://github.com/SamirAtef/islami-app.git>
2022-05-16 11:14:41 +0200

    add category recyclerview

--- app/src/main/java/com/example/ecommerceapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/ecommerceapp/fragments/HomeFragment.java
@@ -6,11 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.ecommerceapp.R;
+import com.example.ecommerceapp.adapters.CategoryAdapter;
+import com.example.ecommerceapp.models.CategoryModel;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +23,10 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView catRecyclerview;
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModelList;
+    FirebaseFirestore db;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -29,6 +38,10 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        catRecyclerview = root.findViewById(R.id.rec_category);
+
+        db = FirebaseFirestore.getInstance();
+
 
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
         List<SlideModel> slideModels = new ArrayList<>();
@@ -37,6 +50,49 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.banner3, "70% OFF", ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+        catRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+
+        categoryModelList = new ArrayList<>();
+        createcollection();
+        categoryAdapter = new CategoryAdapter(getContext(), categoryModelList);
+        catRecyclerview.setAdapter(categoryAdapter);
+        createcollection();
+//        db.collection("Category")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//
+//                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+//                                categoryModelList.add(categoryModel);
+//                                categoryAdapter.notifyDataSetChanged();
+//
+//                            }
+//                        } else {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//
+//                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+//                                categoryModelList.add(categoryModel);
+//
+//                            }
+//
+//                        }
+//                    }
+//                });
         return root;
+    }
+
+    private void createcollection() {
+        categoryModelList.add(new CategoryModel(R.drawable.baby_clothing, "Kids Collection", "kids"));
+        categoryModelList.add(new CategoryModel(R.drawable.camera, "Camera Collection", "camera"));
+        categoryModelList.add(new CategoryModel(R.drawable.dress, "Woman Collection", "woman"));
+        categoryModelList.add(new CategoryModel(R.drawable.shirt, "Man Collection", "man"));
+        categoryModelList.add(new CategoryModel(R.drawable.sneakers, "Shoes Collection", "shoes"));
+        categoryModelList.add(new CategoryModel(R.drawable.watch, "Watch Collection", "watch"));
+
+
     }
 }
